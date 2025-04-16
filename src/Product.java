8a5e8723ec3d94b2892406f7ef9a2f0433ef6a32
Sinhaaayam <135627202+Sinhaaayam@users.noreply.github.com>
2025-04-16 09:53:25 +0530

    Update Product.java

--- src/Product.java
+++ src/Product.java
@@ -1,1 +1,28 @@
+public class Product {
+    // Attributes
+    private int id;
+    private String name;
+    private double price;
 
+    // Constructor
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Method to display product details
+    public void displayDetails() {
+        System.out.println("Product Details:");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+    }
+
+    // Main method to test the Product class
+    public static void main(String[] args) {
+        // Example input
+        Product product = new Product(101, "Laptop", 75000);
+        product.displayDetails();
+    }
+}
